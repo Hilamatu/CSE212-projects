b0cea3784be79786765401ffb33f24c373972002
hilamatu <hil21006@byui.edu>
2025-11-12 07:07:50 +0900

    Problem 4 solved

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -30,36 +30,72 @@ public class Maze
     /// Check to see if you can move left.  If you can, then move.  If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
+    /// Get the current location and check the left direction (index 0) in the boolean array.
+    /// if true, then decrement _currX to move to left, else throw exception.
     public void MoveLeft()
     {
-        // FILL IN CODE
+        if (_mazeMap[(_currX, _currY)][0])
+        {
+            _currX--;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
     /// Check to see if you can move right.  If you can, then move.  If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
+    /// Get the current location and check the right direction (index 1) in the boolean array.
+    /// if true, then increment _currX to move to right, else throw exception.
     public void MoveRight()
     {
-        // FILL IN CODE
+        if (_mazeMap[(_currX, _currY)][1])
+        {
+            _currX++;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
     /// Check to see if you can move up.  If you can, then move.  If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
+    /// Get the current location and check the up direction (index 2) in the boolean array.
+    /// if true, then DECREMENT _currY to move up, else throw exception.
     public void MoveUp()
     {
-        // FILL IN CODE
+        if (_mazeMap[(_currX, _currY)][2])
+        {
+            _currY--;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
     /// Check to see if you can move down.  If you can, then move.  If you
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
+    /// Get the current location and check the left down (index 3) in the boolean array.
+    /// if true, then INCREMENT _currY to move down, else throw exception.
     public void MoveDown()
     {
-        // FILL IN CODE
+        if( _mazeMap[(_currX, _currY)][3])
+        {
+            _currY++;
+        }
+        else
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     public string GetStatus()
