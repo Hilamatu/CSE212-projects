e06aae34368a8cadfbca624069bcb822a3198af2
hilamatu <hil21006@byui.edu>
2025-11-12 08:36:26 +0900

    Problem 5 solved

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,21 @@
+// <summary>
+//Custom class to hold the features from the GeoJSON data
+//Nested classes to represent the structure of the JSON data
+// Each earthquake feature has properties including magnitude and place
+// </summary>
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public List<Feature> Features { get; set; }
 }
+
+public class Feature
+{
+    public Properties Properties { get; set; }
+}
+
+public class Properties
+{
+    public decimal? Mag { get; set; }
+    public string Place { get; set; }
+}
+
